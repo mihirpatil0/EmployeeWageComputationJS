7deb7367fa4a40c9c9113efb130532521158a6c1
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-03 23:03:14 +0530

    [mihir] Added: Employee wage computation problem.

---
+++ EmployeeWageComputation.js
@@ -0,0 +1,95 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
+const MAX_HRS_IN_MONTH = 100;
+
+//uc1
+{
+    const IS_ABSENT = 0;
+
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_ABSENT)
+    {
+        console.log("Employee is Absent");
+    }
+    else
+    {
+        console.log("Employee is present");
+    }
+}
+
+//uc2
+{
+
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs = 0;
+    }
+
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee Wage : " + empWage);
+}
+
+//uc3
+{
+
+    function getWorkingHours(empCheck)
+    {
+        switch(empCheck)
+        {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee Wage: " + empWage);
+}
+
+//uc4
+{
+    let empHrs = 0;
+    for(let day = 0; day < NUM_OF_WORKING_DAYS; day++)
+    {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHrs += getWorkingHours(empCheck);
+    }
+
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Total Hrs : " + empHrs + " Employee Wage : " + empWage);
+}
+
+//uc5
+{
+    const NUM_OF_WORKING_DAY = 10;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY)
+    {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs + " Employee Wage: " + empWage);
+}
