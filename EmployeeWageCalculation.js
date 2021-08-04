19216b3ad69a66d1c623265c2a000039b51677b0
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-04 20:54:38 +0530

    [mihir] Refactored: introduced function in program.

--- EmployeeWageCalculation.js
+++ EmployeeWageCalculation.js
@@ -4,18 +4,20 @@ const PART_TIME_HOURS = 4 ;
 const FULL_TIME_HOURS = 8 ;
 const WAGE_PER_HOUR = 20 ;
 
-let empHours = 0 ;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck)
+function getWorkingHours(empCheck)
 {
-    case IS_PART_TIME:
-        empHours = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        break;
-    default:
-        empHours = 0;
+    switch (empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-let empWage =empHours * WAGE_PER_HOUR ;
-console.log("Employee Wage " + empWage)
+
+let empCheck = Math.floor(Math.random() * 10) % 3 ;
+let empHours = getWorkingHours(empCheck);
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage " + empWage);
