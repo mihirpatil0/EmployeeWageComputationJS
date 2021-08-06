fd41d12f09b3e7bdfb3f16cbe597b6441306b784
mihirpatil0 <mihirrpatil@gmail.com>
2021-08-06 23:30:53 +0530

    [mihir] Added: estended storing gender and start date.

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -3,12 +3,17 @@ class EmployeePayrollData
     //property
     id;
     slary;
+    gender;
+    startDate;
+
     //constructor
-    constructor(id ,name , salary)
+    constructor(...params)
     {
-        this.id =id;
-        this.name = name;
-        this.salary = salary;
+        this.id =params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getters and setters
@@ -18,7 +23,9 @@ class EmployeePayrollData
     //method
     toString()
     {
-        return `id = ${this.id} name = ${this.name} salary = ${this.salary}`
+        const options = {year : 'numeric', month : 'long' , day : 'numeric'}
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us",options);
+        return `id = ${this.id} name = ${this.name} salary = ${this.salary} gender ${this.gender} startDate ${empDate}`
     }
 }
 
@@ -26,3 +33,5 @@ let employeePayrollData = new EmployeePayrollData(1,"Mark",30000)
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "john"
 console.log(employeePayrollData.toString());
+let newemployeePayrollData = new EmployeePayrollData(1,"Teresa",30000,'F',new Date())
+console.log(newemployeePayrollData.toString());
